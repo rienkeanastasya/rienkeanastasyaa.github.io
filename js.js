e051cf61f8189a3b8e5956f3e8ee5146141769a1
Rienke Anastasya <rienkeanastasyawyanti@gmail.com>
2023-10-31 10:40:26 +0700

    first commit

---
+++ js.js
@@ -0,0 +1,20 @@
+const txtElement = ['Rienke Anastasya Wyanti'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+	if(count == txtElement.length){
+		count = 1;
+	}
+
+	currentTxt = txtElement[count];
+
+	words = currentTxt.slice(0, ++txtIndex);
+	document.querySelector('.efek-ngetik').textContent = words;
+
+	setTimeout(ngetik, 500);
+
+})();
